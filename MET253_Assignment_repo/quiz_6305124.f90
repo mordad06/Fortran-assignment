8c06f95b77758db4299b93f313af95ee02fcb3a6
mordad06 <mordeaa06@gmail.com>
2026-04-30 16:16:19 +0000

    Update quiz_6305124.f90

--- MET253_Assignment_repo/quiz_6305124.f90
+++ MET253_Assignment_repo/quiz_6305124.f90
@@ -1,18 +1,27 @@
 PROGRAM quiz
  IMPLICIT NONE
- INTEGER,DIMENSION(10,10)::yy
- yy(1) = 1
- yy(2) = 2
- yy(3) = 3
- yy(4) = 4
- yy(5) = 5
- yy(6) = 6
- yy(7) = 7
- yy(8) = 8
- yy(9) = 9
- yy(10) = 10
- 
- PRINT*,yy
-
+ INTEGER :: i
+ CHARACTER(LEN=1) :: Grade
+ CHARACTER(LEN=11) :: Remarks
+ INTEGER,DIMENSION(10) :: Score
+ Score = (/85,62,45,91,38,74,55,88,61,47/)
+ print*,"STDUENT     SCORE     GRADE     REMARKS"
+do i = 1,10
+  if(Score(i) >=80) then
+     Grade = "A"
+     Remarks = "Distinction"
+  else if(Score(i) >= 60) then
+     Grade = "B"
+     Remarks ="Credit"
+  else if(Score(i) >= 40) then
+     Grade = "C"
+     Remarks = "Pass"
+  else
+     Grade = "F"
+     Remarks = "Fail"
+  end if
+  print("I3,9X,I3,7X,A1,9X,A11"), i, Score(i), Grade, Remarks
+ end do
  END PROGRAM quiz
- 
+ !ADOMAKO MORDECAI ANANE
+ !6305124
